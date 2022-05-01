d1792a07b66dfb2656c602ba74ed997315346107
Teribble <ggameparadox@gmail.com>
2022-05-01 03:08:44 +0300

    Добавьте файлы проекта.

---
+++ BehaviorPatterns/Car.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy
+{
+    public class Car
+    {
+        public int Passengers { get; set; }
+        public string Model { get; set; }
+
+        public IMoveble Moveble { get; set; }
+        public Car(int passengers, string model, IMoveble moveble)
+        {
+            Passengers = passengers;
+
+            Model = model;
+
+            Moveble = moveble;
+        }
+        public void Move()
+        {
+            Moveble.Move();
+        }
+    }
+}
---
+++ BehaviorPatterns/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Strategy
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Car car = new Car(4, "Volvo", new ElecticCar());
+            car.Move();
+            car.Moveble = new GasOilCar();
+            car.Move();
+        }
+    }
+}
